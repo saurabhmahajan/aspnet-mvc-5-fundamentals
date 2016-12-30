c1a6ae2f5cc77d4ebe005380ed87e03bf15704bd
Saurabh Mahajan <saurabh7820@gmail.com>
2016-12-30 18:42:42 +0530

    Initial commit

---
+++ OwinConsoleApp/OwinConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OwinConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
